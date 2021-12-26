20e61c35cd4f1cfe52506c0c8fd51a15c8f24dba
Carlos Lopez <carlos@wecancodeit.org>
2021-12-26 18:14:12 -0500

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+
+// Use this code if you choose to do Node JS
+
+export default() => {
+    const appDiv = document.getElementById("app");
+    appDiv.innerHTML = `<h1>Hello World!</h1>`;
+}
+
+
+// Simply use this as your main JS if you want to use Vanilla JS
